2b6963bba8b5f95bd18ac43731f11f069c95052b
Gouthami S Kannan <gouthami.senthamarai@ucalgary.ca>
2016-07-28 19:15:56 -0600

    Update emR_bootstrap.sh

--- R/Hadoop/emR_bootstrap.sh
+++ R/Hadoop/emR_bootstrap.sh
@@ -99,8 +99,8 @@ sudo sh -c "echo '$USERPW' | passwd $USER --stdin"
 if [ "$IS_MASTER" = true -a "$RSTUDIO" = true ]; then
   # install Rstudio server
   # please check and update for latest RStudio version
-  wget http://download2.rstudio.org/rstudio-server-0.98.983-x86_64.rpm
-  sudo yum install --nogpgcheck -y rstudio-server-0.98.983-x86_64.rpm
+  wget https://download2.rstudio.org/rstudio-server-0.99.903-amd64.deb
+  sudo yum install --nogpgcheck -y rstudio-server-0.99.903-amd64.deb
   
   # change port - 8787 will not work for many companies
   sudo sh -c "echo 'www-port=$RSTUDIOPORT' >> /etc/rstudio/rserver.conf"
